e38a89d966c955f2df2fccda83c12724dee18c3c
Jonas-S-D <144025767+Jonas-S-D@users.noreply.github.com>
2025-05-04 15:58:00 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -60,7 +60,7 @@ function loadContentFromLogo() {
 
 async function loginUser(email, password) {
     try {
-        const response = await fetch('/auth/login', {
+        const response = await fetch('api/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
