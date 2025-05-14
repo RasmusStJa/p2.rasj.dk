7696e1370ce0380cc29bb3f42faa9417d43cd405
Jonas-S-D <144025767+Jonas-S-D@users.noreply.github.com>
2025-05-14 23:15:50 +0200

    Update post.routes.ts

--- backend/post/post.routes.ts
+++ backend/post/post.routes.ts
@@ -1,12 +1,12 @@
 import express, { RequestHandler } from 'express';
-import { createPost, likePost, commentOnPost } from './post.controller';
+import { createPost, reactToPost, commentOnPost } from './post.controller';
 
 const router = express.Router();
 
 router.post('/', createPost as RequestHandler);
 
-// Like a post
-router.post('/:id/like', likePost as RequestHandler);
+// React to a post
+router.post('/:id/react', reactToPost as RequestHandler);
 
 // Comment on a post
 router.post('/:id/comment', commentOnPost as RequestHandler);
